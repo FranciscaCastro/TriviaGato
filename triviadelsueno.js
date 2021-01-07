3d7838f1a1ea746f87318fb0f02d50de1a28b831
Francisca Castro <kikazauriawong@gmail.com>
2021-01-07 03:41:04 +0000

    ahora si que si

--- triviadelsueno.js
+++ triviadelsueno.js
@@ -42,6 +42,28 @@ function iniciarCronometro() {
   
 }
 
+function iniciarTiempo(duracion, componente) {
+    interval = setInterval(() => {
+    if (duracion === 0) {
+
+      componente.innerHTML = "Se acabó el tiempo";
+
+      clearInterval(interval);
+
+      loadQuestions()
+
+    } else {
+      
+      duracion = duracion <45 ? + duracion : duracion;
+
+      componente.textContent = "00:" + duracion;
+
+      duracion--;
+    }
+    }, 1000)
+
+}
+
 function loadQuestions() {
    iniciarCronometro();
 
--- triviarandom.js
+++ triviarandom.js
@@ -35,6 +35,7 @@ function iniciar() {
     }
     }
 
+
 function loadQuestions() {
    iniciarCronometro();
 
